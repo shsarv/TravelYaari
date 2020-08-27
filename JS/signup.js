93b22a00a2a4e125a87cd7ce9f9ba5b29c64648c
Sarvesh Kumar Sharma <shsarv@gmail.com>
2020-08-27 13:03:24 +0530

    added js files

---
+++ JS/signup.js
@@ -0,0 +1,71 @@
+const form = document.getElementById("signup-form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = "signup-form-control error";
+    const small = formControl.querySelector("small");
+    small.innerText = message;
+}
+
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = "signup-form-control success";
+}
+
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, "Email is not valid");
+    }
+}
+
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function checkRequired(inputArray) {
+    inputArray.forEach(function(input) {
+        if (input.value.trim() === "") {
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(
+            input,
+            `${getFieldName(input)} must be atleast ${min} characters`
+        );
+    } else if (input.value.length > max) {
+        showError(
+            input,
+            `${getFieldName(input)} must be less than ${max} characters`
+        );
+    } else {
+        showSuccess(input);
+    }
+}
+
+function checkPassword(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, "Passwords do not match");
+    }
+}
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 20);
+    checkEmail(email);
+    checkPassword(password, password2);
+});
--- JS/team.js
+++ JS/team.js
@@ -3,7 +3,7 @@ let i = 0;
 
 const typing = () => {
     if (i < heading.length) {
-        document.querySelector('.heading').innerHTML += heading.charAt(i);
+        document.querySelector('.team-heading').innerHTML += heading.charAt(i);
         i++;
 
         setTimeout(typing, 150);
