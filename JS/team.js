55ce3e770ee761c9c79b209a7a220064337a73f9
Sarvesh Kumar Sharma <shsarv@gmail.com>
2020-08-27 09:43:13 +0530

    created team

---
+++ JS/team.js
@@ -0,0 +1,13 @@
+const heading = 'Please Meet Our Team';
+let i = 0;
+
+const typing = () => {
+    if (i < heading.length) {
+        document.querySelector('.heading').innerHTML += heading.charAt(i);
+        i++;
+
+        setTimeout(typing, 150);
+    }
+}
+
+typing();
